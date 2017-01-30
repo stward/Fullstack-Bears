77611737b8ce176786a01a29149ddd171ea40229
fresh5447 <doug@kosmojo.com>
2017-01-30 11:44:48 -0700

    create endpoint utilizing the req params object

--- server.js
+++ server.js
@@ -5,13 +5,18 @@ app.get('/api/greeting', function(req, res) {
   res.send("Happy Monday!");
 });
 
+app.get('/api/greeting/:name', function(req, res) {
+  var name = req.params.name;
+  res.send("Happy Monday " + name + "!");
+});
+
 app.get('/api/happy', function(req, res) {
   res.send("I Love Mondays!");
 });
 
 app.get('/api/sad', function(req, res) {
-  res.send("Mondays are moderately tollerable.")
-})
+  res.send("Mondays are moderately tollerable.");
+});
 
 app.listen(3000, function(){
   console.log("Express application fired up on port 3000");
