ed59160ef0a925f71649b58ef02d070f22cbd14c
fresh5447 <doug@kosmojo.com>
2017-01-30 14:49:01 -0700

    implement POST method for bears resource

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
 var express = require('express');
 var app = express();
 var Bear = require('./models/bear');
-
+var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost/fullstack-bears");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/api/bears', function(req, res){
   res.json({ message: "You found the bears route!" });
 });
+
+app.post('/api/bears', function(req, res) {
+
+  //make new bear using mongoose schema
+  var bear = new Bear({
+    age:      req.body.age,
+    species:  req.body.species,
+    name:     req.body.name,
+    weight:   req.body.weight ,
+    location: req.body.location,
+    attitude: req.body.attitude
+  });
+
+  // save bear with node err style callback
+  bear.save(function(err, bearData) {
+    if(err){
+      console.log(err, "Error with your bearer");
+    } else {
+      res.json(bearData);
+    }
+  });
+
+});
+
 
 app.listen(3000, function(){
   console.log("lets get 👾 👾 👾 👾 🔥 🎮 🔥 🎮 🔥 🎮 🔥 🎮 🔥 🎮 🔥 🎮 🔥 🎮 🔥 🎮 🔥 👾 👾 👾 👾 up on port 3000");
