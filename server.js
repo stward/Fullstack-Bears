b03236b3c9768e69d9d98e3393297ca5bedc8aa5
fresh5447 <doug@kosmojo.com>
2017-01-30 11:30:43 -0700

    added sample api endpoints utilizing GET method

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ app.get('/api/greeting', function(req, res) {
   res.send("Happy Monday!");
 });
 
+app.get('/api/happy', function(req, res) {
+  res.send("I Love Mondays!");
+});
+
+app.get('/api/sad', function(req, res) {
+  res.send("Mondays are moderately tollerable.")
+})
+
 app.listen(3000, function(){
   console.log("Express application fired up on port 3000");
 });
