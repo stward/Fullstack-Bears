53e8af8677150483b3f4e6e34b11ee68c2a81e0d
fresh5447 <doug@kosmojo.com>
2017-01-30 14:19:18 -0700

    defined mongoose schema for bear, which is the blueprint for our data model

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose');
+var Bear = require('./models/bear');
 
-// will connect to mongo db with title 'fullstack-bears'
+
+var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost/fullstack-bears");
 
 app.get('/api/bears', function(req, res){
-  res.json({message: "You found the bears route!"});
+  res.json({ message: "You found the bears route!" });
 });
 
 app.listen(3000, function(){
-  console.log("Express application fired up on port 3000");
+  console.log("lets get 👾 👾 👾 👾 🔥 🎮 🔥 🎮 🔥 🎮 🔥 🎮 🔥 🎮 🔥 🎮 🔥 🎮 🔥 🎮 🔥 👾 👾 👾 👾 up on port 3000");
 });
