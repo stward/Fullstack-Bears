1d96724541dcfcb741cc32624da8c3934903dcb1
fresh5447 <doug@kosmojo.com>
2017-01-30 10:50:37 -0700

    used node http module to spin up simple server

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+var http = require('http');
+
+http.createServer( function(req, res) {
+  res.send("Hello World!!!");
+}).listen(3000);
+
+
+console.log("Server is up and running!");
