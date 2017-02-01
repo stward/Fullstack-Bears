115f465b91ef9f9d9d9cff4cb4a74fb6165b192a
Carl A. Fahlstrom <carl.fahlstrom@gmail.com>
2017-02-01 07:06:36 -0700

    Able to delete bears, started working on updating bears

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   console.log("We are linked to the static js file.");
 
   $(".delete").on('click', function () {
-    var bearId = $(this).attr('id');
+    var bearId = $(this).attr('id').slice(4);
     console.log(bearId);
     $.ajax({
       url: '/api/bears/'+bearId,
@@ -10,6 +10,40 @@ $(document).ready(function() {
     }).done(function (data) {
       console.log(data);
       window.location = "/bears";
+    });
+  });
+
+  $('#newBear').submit(function () {
+    var name = $("#name").val();
+    var species = $('#species').val();
+    var age = $('#age').val();
+    var weight = $('#weight').val();
+    var location = $('#location').val();
+    var attitude = $('#attitude').val();
+
+    $.ajax({
+      url: '/api/bears',
+      method: 'POST',
+      data: {
+        name: name,
+        species: species,
+        age: age,
+        weight: weight,
+        location: location,
+        attitude: attitude
+      }
+    }).done(function (data) {
+      console.log(data);
+      window.location = "/bears";
+    })
+  });
+
+  $('.update').on('click', function () {
+    var bearId = $(this).attr('id').slice(7);
+    console.log(bearId);
+    $.get('/api/bears/'+bearId).done(function (data) {
+      console.log(data);
+      $('#name').val(data.name);
     });
   });
 
